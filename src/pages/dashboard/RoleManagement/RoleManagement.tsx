3c4279664e28e011d99d0b3976b8bb7d312cf362
aayushhh44 <aayushpoudel59@gmail.com>
2024-04-08 16:30:21 +0545

    Add: RoleManagement section

--- src/pages/dashboard/RoleManagement/RoleManagement.tsx
+++ src/pages/dashboard/RoleManagement/RoleManagement.tsx
@@ -1,39 +1,223 @@
-import { Box, Table } from "@mantine/core";
+import { Box, Button, Group, Paper, Table, useMantineTheme } from "@mantine/core";
+import { Avatar, Tooltip } from "@mantine/core";
+import theme from "../../../theme";
 
 const elements = [
-    { sn: 1, mass: 12.011, symbol: 'C', name: 'Carbon' },
-    { sn: 2, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
-    { sn: 3, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
-    { sn: 4, mass: 137.33, symbol: 'Ba', name: 'Barium' },
-    { sn: 5, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
-  ];
-  
-const RoleManagement = () => {
-    const rows = elements.map((element) => (
-        <Table.Tr key={element.name}>
-          <Table.Td>{element.sn}</Table.Td>
-          <Table.Td>{element.name}</Table.Td>
-          <Table.Td>{element.symbol}</Table.Td>
-          <Table.Td>{element.mass}</Table.Td>
-        </Table.Tr>
-      ));
+    {
+        sn: 1,
+        Roles: "Team Lead",
+        user: [
+            {
+            id: 1,
+            name: "Aayush",
+            img: "..."
+        },
+        
+        {
+            id: 2,
+            name: "Rahul",
+            img: "..."
+        }, {
+            id: 3,
+            name: "Sita",
+            img: "https://wallpapers.com/images/hd/portrait-of-woman-with-random-people-5wu04gyr7p6p0i5c.jpg"
+        }
+        ]
+    },{
+        sn: 2,
+        Roles: "Unit Coordinator",
+        user: [
+            {
+            id: 1,
+            name: "Aayush",
+            img: "..."
+        },
+        
+        {
+            id: 2,
+            name: "Rahul",
+            img: "..."
+        }, {
+            id: 3,
+            name: "Sita",
+            img: "https://wallpapers.com/images/hd/portrait-of-woman-with-random-people-5wu04gyr7p6p0i5c.jpg"
+        }
+        ]
+    },{
+        sn: 3,
+        Roles: "Data Entry",
+        user: [
+            {
+            id: 1,
+            name: "Aayush",
+            img: "..."
+        },
+        
+        {
+            id: 2,
+            name: "Rahul",
+            img: "..."
+        }, {
+            id: 3,
+            name: "Sita",
+            img: "https://wallpapers.com/images/hd/portrait-of-woman-with-random-people-5wu04gyr7p6p0i5c.jpg"
+        }
+        ]
+    },{
+        sn: 4,
+        Roles: "Finance",
+        user: [
+            {
+            id: 1,
+            name: "Aayush",
+            img: "..."
+        },
+        
+        {
+            id: 2,
+            name: "Rahul",
+            img: "..."
+        }, {
+            id: 3,
+            name: "Sita",
+            img: "https://wallpapers.com/images/hd/portrait-of-woman-with-random-people-5wu04gyr7p6p0i5c.jpg"
+        }
+        ]
+    },{
+        sn: 5,
+        Roles: "Sales",
+        user: [
+            {
+            id: 1,
+            name: "Aayush",
+            img: "..."
+        },
+        
+        {
+            id: 2,
+            name: "Rahul",
+            img: "..."
+        }, {
+            id: 3,
+            name: "Sita",
+            img: "https://wallpapers.com/images/hd/portrait-of-woman-with-random-people-5wu04gyr7p6p0i5c.jpg"
+        }
+        ]
+    }
+    ,{
+        sn: 6,
+        Roles: "Call Center",
+        user: [
+            {
+            id: 1,
+            name: "Aayush",
+            img: "..."
+        },
+        
+        {
+            id: 2,
+            name: "Rahul",
+            img: "..."
+        }, {
+            id: 3,
+            name: "Sita",
+            img: "https://wallpapers.com/images/hd/portrait-of-woman-with-random-people-5wu04gyr7p6p0i5c.jpg"
+        }
+        ]
+    }
+    ,{
+        sn: 7,
+        Roles: "IT Team",
+        user: [
+            {
+            id: 1,
+            name: "Aayush",
+            img: "..."
+        },
+        
+        {
+            id: 2,
+            name: "Rahul",
+            img: "..."
+        }, {
+            id: 3,
+            name: "Sita",
+            img: "https://wallpapers.com/images/hd/portrait-of-woman-with-random-people-5wu04gyr7p6p0i5c.jpg"
+        }
+        ]
+    }
+    ,{
+        sn: 8,
+        Roles: "Custom",
+        user: [
+            {
+            id: 1,
+            name: "Aayush",
+            img: "..."
+        },
+        
+        {
+            id: 2,
+            name: "Rahul",
+            img: "..."
+        }, {
+            id: 3,
+            name: "Sita",
+            img: "https://wallpapers.com/images/hd/portrait-of-woman-with-random-people-5wu04gyr7p6p0i5c.jpg"
+        }
+        ]
+    }
+];
 
-   
-  return (
-    <Box>
-    <Table>
-      <Table.Thead>
-        <Table.Tr>
-          <Table.Th>S.N.</Table.Th>
-          <Table.Th>Element name</Table.Th>
-          <Table.Th>Symbol</Table.Th>
-          <Table.Th>Atomic mass</Table.Th>
+const RoleManagement = () => {
+
+    const rows = elements.map((element) => (
+        <Table.Tr key={element.sn}>
+            <Table.Td>{element.sn}</Table.Td>
+            <Table.Td>{`${element.Roles}(${element.user.length})`}</Table.Td>
+            <Table.Td>
+                <Tooltip.Group openDelay={300} closeDelay={100}>
+                    <Avatar.Group spacing="sm">
+                        {element.user.map((item) => {
+                            return (
+                                <Tooltip label={item.name} withArrow>
+                                    <Avatar src={item.img} radius="xl" />
+                                </Tooltip>
+                            )
+                        })}
+                    </Avatar.Group>
+                </Tooltip.Group>
+            </Table.Td>
+            <Table.Td><Button variant="transparent"><u>View More</u></Button></Table.Td>
         </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>{rows}</Table.Tbody>
-    </Table>
-    </Box>
-  )
+    ));
+
+
+    return (
+
+        <Paper withBorder p="xl">
+        {/* <Box my="xl" p="lg"> */}
+            <Group justify="space-between">
+                <h3>Members</h3>
+                <Group>
+                    <Button variant="outline" color="green-color.0">Asigned Member</Button>
+                    <Button color="green-color.0">Asigned Role</Button>
+                </Group>
+            </Group>
+            <Table horizontalSpacing="xl" withRowBorders={false}>
+                <Table.Thead>
+                    <Table.Tr>
+                        <Table.Th>S.N.</Table.Th>
+                        <Table.Th>Roles</Table.Th>
+                        <Table.Th>Users</Table.Th>
+                        <Table.Th>Action</Table.Th>
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>{rows}</Table.Tbody>
+            </Table>
+        {/* </Box> */}
+        </Paper>
+    )
 }
 
 export default RoleManagement
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,10 @@
 
 const theme = {
+    primaryColor: "primary-blue",
+    colors: {
+        "green-color": ["green",'#5FCCDB', '#44CADC', '#2AC9DE', '#1AC2D9', '#11B7CD', '#09ADC3', '#0E99AC', '#128797', '#147885'],
+        "primary-blue": ['#6092FE','#6092FE', '#6092FE', '#6092FE', '#6092FE', '#6092FE', '#6092FE', '#6092FE', '#6092FE', '#6092FE']
+    },
     breakpoints: {
         xs: "0",
         sm: "576",
