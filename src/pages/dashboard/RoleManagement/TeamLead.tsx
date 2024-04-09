a5e3a8040d09bb861dbf6aa322849e85ff4bbed3
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-04-09 11:43:31 +0545

    added Assignmember

--- src/pages/dashboard/RoleManagement/TeamLead.tsx
+++ src/pages/dashboard/RoleManagement/TeamLead.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { Button, Flex, Image, Paper, TextInput, Title } from '@mantine/core'
 
 const TeamLead = () => {
-  return (
-    <div>TeamLead</div>
-  )
+    return (
+        <>
+            <Title c='blue.5'>Role Management</Title>
+
+            <Paper>
+                {/* <Flex>
+                    <Title> Team(3)</Title>
+                    
+                        <TextInput
+                            bg={'smokewhite'}
+                            placeholder='search by Name or Email'
+                        />
+                        <Button bg={'smokewhite'} variant='default'>Filter</Button>
+                   
+                </Flex> */}
+                <Flex>
+                <Flex>
+                    <Image
+                        h={100}
+                        w={100}
+                        radius="md"
+                        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
+                    />
+                </Flex>
+                <Flex>
+                    <Image
+                        h={100}
+                        w={100}
+                        radius="md"
+                        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
+                    />
+                </Flex>
+
+                <Flex>
+                    <Image
+                        h={100}
+                        w={100}
+                        radius="md"
+                        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
+                    />
+                </Flex>
+                </Flex>
+
+
+            </Paper>
+        </>
+    )
 }
 
 export default TeamLead
